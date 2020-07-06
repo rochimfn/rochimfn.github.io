feb05fdf4fb1958f63934293e44b1fd9cecfeae5
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2020-07-06 11:24:56 +0700

    Updates

---
+++ component---src-templates-blog-post-js-0314926304c56436cd57.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{yZlL:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return u})),e.d(n,"query",(function(){return l}));e("q1tI");var r=e("Bl7J"),a=e("qKvR");function u(t){var n=t.data.markdownRemark;return Object(a.b)(r.a,{title:n.frontmatter.title},Object(a.b)("div",null,Object(a.b)("h1",null,n.frontmatter.title),Object(a.b)("div",{dangerouslySetInnerHTML:{__html:n.html}})))}var l="471609186"}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-0314926304c56436cd57.js.map
