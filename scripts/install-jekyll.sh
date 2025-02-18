4459815cf6d6a6f5ccb87dd5f05f8d0b7aab2c96
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2025-02-18 13:50:30 +0700

    Add helper script for jekyll installation

---
+++ scripts/install-jekyll.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+sudo apt install -y ruby-full build-essential zlib1g-dev
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo ""
+    echo "error install ruby"
+    exit $retVal
+fi
+
+echo ""
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
+gem install bundler
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo ""
+    echo "error install bundler"
+    exit $retVal
+fi
+
+echo ""
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$SCRIPT_DIR/../
+cd $PROJECT_DIR
+bundle install
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo ""
+    echo "error install dependencies"
+    exit $retVal
+fi
+
+echo ""
+echo "run below command to start jekyll:"
+echo ""
+echo "source ~/.bashrc"
+echo "bundle exec jekyll serve"
+echo ""
